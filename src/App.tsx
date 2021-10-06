974dbbcd867fc89067f7fa8f7cd1648f6f40ca06
Abin Joseph <itsmeabjs@gmail.com>
2021-10-06 14:16:01 +0530

    V.0.1 is Completed
    Hack CLub KMEA Review App with React V.0.1  is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useEffect, useState } from "react";
+import "./App.css";
+import logo_github from "./images/github.png";
+export default function App() {
+  const [reviewData, setReviewData] = useState<ReviewComponentProps[] | []>([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/hackclubkmea/hackclubkmeareview/main/review.json"
+      );
+      const data = await response.json();
+      const dataArray = Object.keys(data).map((key) => data[key]);
+      setReviewData(dataArray);
+    }
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__container">
+      <div className="app_review__component">
+        {reviewData.map((review, index) => (
+          <ReviewComponent {...review} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+interface ReviewComponentProps {
+  name: string;
+  github: string;
+  review: string;
+}
+
+export const ReviewComponent = ({
+  name,
+  github,
+  review,
+}: ReviewComponentProps) => {
+  return (
+    <div className="review">
+      <div className="review__component_name_and_avatar">
+        <div className="avatar_component">
+          <img src={`${github}.png`} alt={"Github profile image of " + name} />
+        </div>
+        <div className="name_and_logo">
+          <a href={github} target="_blank" rel="noopener noreferrer">
+            <img src={logo_github} alt="Github Logo" />
+            <div>
+              <strong>{name}</strong>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="review__concent">{review}</div>
+    </div>
+  );
+};
